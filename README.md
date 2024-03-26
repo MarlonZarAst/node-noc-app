f11ad4be97066e1a6168632c4372c63d083f60f5
Marlon <marlon.zarate@pidelectronics.com>
2024-03-26 11:44:29 -0600

    Se añadio clean architecture de repositorio seccion 9

---
+++ README.md
@@ -0,0 +1,17 @@
+# Proyecto NOC
+
+EL objetivo de este proyecto es crear una serie de tareas utilizando arquitectura limpia con typescript
+
+# dev
+1. Clonar el archivo .env.template en .env
+2. Configurar las variables de entorno
+
+```
+PORT=3001
+
+MAILER_EMAIL=
+MAILER_SECRET_KEY=
+
+PROD=
+
+```
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,15 +1,23 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repository/log-impl.repository";
 import { CronService } from "./cron/cron-service";
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource()
+);
 
 export class Server {
 
   public static start() {
     console.log('Server started...');
+    const url = 'http://localhost:3000/';
     CronService.createJob('*/5 * * * * *', () => {
       new CheckService(
-        () => console.log('Success'),
-        (error) => console.log(error)
-      ).execute('https://www.google.com/');
+        fileSystemLogRepository,
+        undefined,
+        undefined
+      ).execute(url);
       // new CheckService().execute('http://localhost:3000/');
     });
   }
